517ebdfaf5516bd5f6958f4fa1c709326355d7f0
UDav <iadavidov89@gmail.com>
2013-03-31 19:21:16 +0700

    change service

--- src/com/udav/mybus/MyBusService.java
+++ src/com/udav/mybus/MyBusService.java
@@ -18,45 +18,48 @@ public class MyBusService extends Service {
 	}
 	
 	public int onStartCommand(Intent intent, int flags, int startId){
-		int command = intent.getIntExtra("command", 0);
+		int answer = intent.getIntExtra("command", 0);
 		link = intent.getStringExtra("link");
-		i = new Intent("com.sonyericsson.extras.liveview.plugins.sandbox");
+		i = new Intent("com.sonyericsson.extras.liveview.plugins.mybus");
 		
 		parser = new Parser(getBaseContext());
-		
-		if (command != 0 ) {
-			Cursor mCursor = DBHelper.getDB(getBaseContext()).query("Bookmark", null, null, null, null, null, null);
-			String titleArr[] = new String[mCursor.getCount()];
-			String linkArr[] = new String[mCursor.getCount()];
-			int counter = 0;
-			if (mCursor.moveToFirst() ) {
-		    	do {
-		    		titleArr[counter] = mCursor.getString(mCursor.getColumnIndex("title"));
-		    		linkArr[counter] =  mCursor.getString(mCursor.getColumnIndex("link"));
-		    		counter++;
-		    	} while(mCursor.moveToNext());
-		    }
-			Bundle b = new Bundle();
-			b.putStringArray("titleArr", titleArr);
-			b.putStringArray("linkArr", linkArr);
-			b.putString("result", "");
-			i.putExtras(b);
 			
-			sendBroadcast(i);
-		} //else
-		if (link != null) {
-			//launch another thread		
-			new Thread() {
-				@Override
-				public void run() {
-					result = parser.parseTime(link);
-					i.putExtra("result", result);
-					sendBroadcast(i);
-					super.run();
-				}
-			}.start();
+		Cursor mCursor = DBHelper.getDB(getBaseContext()).query("Bookmark", null, null, null, null, null, null);
+		String titleArr[] = new String[mCursor.getCount()];
+		String linkArr[] = new String[mCursor.getCount()];
+		int counter = 0;
+		if (mCursor.moveToFirst() ) {
+		   	do {
+		   		titleArr[counter] = mCursor.getString(mCursor.getColumnIndex("title"));
+		   		linkArr[counter] =  mCursor.getString(mCursor.getColumnIndex("link"));
+		   		counter++;
+		   	} while(mCursor.moveToNext());
 		}
-			System.out.println("At application all ok!");
+		Bundle b = new Bundle();
+		b.putStringArray("titleArr", titleArr);
+		b.putStringArray("linkArr", linkArr);
+		b.putString("result", "");
+		i.putExtras(b);
+			
+		switch (answer) {
+			case 0:
+				i.putExtra("command", 0);
+				sendBroadcast(i);
+				break;
+			case 1:
+				new Thread() {
+					@Override
+					public void run() {
+						result = parser.parseTime(link);
+						i.putExtra("command", 1);
+						i.putExtra("result", result);
+						sendBroadcast(i);
+						super.run();
+					}
+				}.start();
+				break;
+		}	
+
 		return super.onStartCommand(intent, flags, startId);
 	}
 
